667740f4aed2849560af21f989f8b9112f4baf4a
Dhialick Ferraz <dhialick.n21@outlook.com>
2025-07-10 12:19:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
-import requests, os, random
+import requests, os, random, time
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 
 rawg_api_key = os.getenv("RAWG_API_KEY")
 
@@ -14,7 +20,7 @@ def get_rawg_data(endpoint_path):
         endpoint_url = (f"https://api.rawg.io/api/{endpoint_path}?key={rawg_api_key}&page={page}&page_size=100")
         
         try:
-            response = requests.get(endpoint_url)
+            response = requests.get(endpoint_url, headers=HEADERS)
             response.raise_for_status()
             raw_data_map = response.json()
             
@@ -31,6 +37,7 @@ def get_rawg_data(endpoint_path):
                     
             has_next = bool(raw_data_map.get("next"))
             page += 1
+            time.sleep(1)
             
         except requests.exceptions.RequestException as e:
             print(f"erro na requisição: {e}")
@@ -53,7 +60,7 @@ def get_game_list(escolhas):
             base_url += f"&{chave}={item}"
       
     try:
-        response = requests.get(base_url)
+        response = requests.get(base_url, headers=HEADERS)
         response.raise_for_status()
         raw_game_list = response.json()
         raw_game_list = raw_game_list.get("results", [])
@@ -68,24 +75,7 @@ def homepage():
     game_recommendation = None
     
     if request.method == "POST":
-        nome_genero = request.form.get("genre")
-        nome_plataforma = request.form.get("platform")
-        nome_publisher = request.form.get("publisher")
-        nome_tag = request.form.get("tag")
-        nome_store = request.form.get("store")
-        
-        id_genero = lista_de_generos.get(nome_genero)
-        id_plataforma = lista_de_plataformas.get(nome_plataforma)
-        id_publisher = lista_de_publisher.get(nome_publisher)
-        id_tag = lista_de_tags.get(nome_tag)
-        id_store = lista_de_lojas.get(nome_store)
-        
-        escolhas = {"genres": id_genero, "platforms": id_plataforma,"publishers": id_publisher, "tags": id_tag, "stores": id_store}
-        
-        raw_game_data = get_game_list(escolhas)
-        game_recommendation = None    
-        if raw_game_data:
-            game_recommendation = random.choice(raw_game_data)        
+     
     
         return render_template("index.html", generos=sorted(lista_de_generos),
                                             plataformas=sorted(lista_de_plataformas),

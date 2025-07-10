1e7ad182528ddf9fc5ef36b18501799c91b649c5
Dhialick Ferraz <dhialick.n21@outlook.com>
2025-07-10 13:53:27 -0300

    Correção de tags e publisher
    Alterei os dicionarios para não depender mais da API, pois estava retornando erro 502 com o número de requisições

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import requests, os, random, time
+import requests, os, random, time, json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -47,8 +47,9 @@ def get_rawg_data(endpoint_path):
 
 lista_de_generos = get_rawg_data("genres")
 lista_de_plataformas = get_rawg_data("platforms")
-lista_de_publisher = get_rawg_data("publishers")
-lista_de_tags = get_rawg_data("tags")
+lista_de_publisher ={'Electronic Arts': 354, 'SEGA': 3408, 'Nintendo': 10681, 'Ubisoft Entertainment': 918, 'Konami': 10691, 'Activision Blizzard': 10830, 'Capcom': 2150, 'Bandai Namco Entertainment': 8352, 'Square Enix': 308, 'Atari': 1779, 'Microsoft Studios': 20987, 'THQ': 6062, 'Sony Computer Entertainment': 10212, 'Disney Interactive': 250, 'Alawar Entertainment': 3693, 'Sony Interactive Entertainment': 11687, 'Paradox Interactive': 3656, 'Plug In Digital': 515, 'PlayWay': 10392, 'Strategy First': 4253, 'Sierra On-Line': 11434, 'Atlus': 9065, 'THQ Nordic': 1283, 'Interplay Productions': 11483, 'SNK': 11893, 'Infogrames': 11453, '2K Games': 358, 'Namco': 10901, 'Warner Bros. Interactive': 350, 'Sekai Project': 3169, 'Microids': 1287, 'Taito': 10867, 'Majesco Entertainment': 1034, 'Acclaim Entertainment': 11433, 'D3 Publisher': 10782, 'Deep Silver': 311, 'Bethesda Softworks': 339, 'Devolver Digital': 1307, 'Koei Tecmo Games': 14676, '505 Games': 243, 'NIS America': 10695, '1C Company': 3370, 'Codemasters': 1294, 'Activision Value Publishing': 11620, 'Slitherine': 14672, 'Gameloft': 35, 'Team17 Digital': 402, 'BANDAI NAMCO Entertainment US': 31896, 'Kiss': 9835, 'Focus Home Interactive': 713}
+lista_de_tags = {'Singleplayer': 31, '2D': 45, 'Pixel Graphics': 122, '3D': 571, 'Short': 111, 'Horror': 16, 'Steam Achievements': 40847, 'Space': 25, 'Retro': 74, 'Multiplayer': 7, 'Cute': 88, 'Atmospheric': 13, 'First-Person': 8, 'Fantasy': 64, 'fun': 2590, 'Top-Down': 61, 'Funny': 4, 'Exploration': 6, 'Colorful': 165, 'Music': 136, 'Story Rich': 118, 'RPG': 24, 'Steam Cloud': 40849, 'Full controller support': 40836, 'Family Sharing': 91686, 'Sci-fi': 32, 'Physics': 114, 'Relaxing': 138, 'Action-Adventure': 69, 'Early Access': 14, 'Dark': 41, 'Minimalist': 112, 'Anime': 134, 'Mystery': 117, 'friends': 744, 'combat': 1465, 'Point & Click': 141, 'Female Protagonist': 189, 'FPS': 30, 'Roguelike': 639, 'Comedy': 123, 'Controller': 115, 'Difficult': 49, 'Third Person': 149, 'Local Multiplayer': 72, 'Co-op': 18, 'Partial Controller Support': 40845, 'VR': 33, 'challenge': 1863, "Shoot 'Em Up": 56}
+
 lista_de_lojas = get_rawg_data("stores")
 
 def get_game_list(escolhas):
@@ -75,7 +76,24 @@ def homepage():
     game_recommendation = None
     
     if request.method == "POST":
-     
+        nome_genero = request.form.get("genre")
+        nome_plataforma = request.form.get("platform")
+        nome_publisher = request.form.get("publisher")
+        nome_tag = request.form.get("tag")
+        nome_store = request.form.get("store")
+        
+        id_genero = lista_de_generos.get(nome_genero)
+        id_plataforma = lista_de_plataformas.get(nome_plataforma)
+        id_publisher = lista_de_publisher.get(nome_publisher)
+        id_tag = lista_de_tags.get(nome_tag)
+        id_store = lista_de_lojas.get(nome_store)
+        
+        escolhas = {"genres": id_genero, "platforms": id_plataforma,"publishers": id_publisher, "tags": id_tag, "stores": id_store}
+        
+        raw_game_data = get_game_list(escolhas)
+        game_recommendation = None    
+        if raw_game_data:
+            game_recommendation = random.choice(raw_game_data)        
     
         return render_template("index.html", generos=sorted(lista_de_generos),
                                             plataformas=sorted(lista_de_plataformas),
